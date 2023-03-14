a2b025feeab1a95dd517c97115111edd4abd2bba
CaptureB <airdavid62@gmail.com>
2023-03-14 15:12:57 -0600

    Finished database.js currently working on webpack

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  // Using the add() method to add some content to the database
+  const request = store.add({id:id, jate:content});
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB('jate',1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  // Using the getAll() method to get all data in the database
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
 
+// Start Database
 initdb();
